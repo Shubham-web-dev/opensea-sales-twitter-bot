c8a1588535c514897f1b49a13cb69cfd0d643879
Shubham Sakharkar <shubhamrsakharkar@gmail.com>
2021-09-09 10:59:55 +0530

    updated code

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const _ = require('lodash');
 const moment = require('moment');
 const { ethers } = require('ethers');
 const tweet = require('./tweet');
-const cache = require('./cache');
 
 // Format tweet text
 function formatAndSendTweet(event) {
@@ -12,36 +11,24 @@ function formatAndSendTweet(event) {
     const totalPrice = _.get(event, 'total_price');
     const usdValue = _.get(event, ['payment_token', 'usd_price']);
     const tokenSymbol = _.get(event, ['payment_token', 'symbol']);
-
     const isEthSale = (tokenSymbol === 'WETH' || tokenSymbol === 'ETH');
     const formattedEthPrice = ethers.utils.formatEther(totalPrice.toString());
     const formattedUsdPrice = (formattedEthPrice * usdValue).toFixed(2);
 
-    // Don't tweet out sales below 2 ETH (preference, can be changed)
-    // if (Number(formattedEthPrice) < 2) {
-    //     console.log(`${tokenName} sold for ${formattedEthPrice}${ethers.constants.EtherSymbol}, below tweet price`);
-    //     return;
-    // }
 
     let tweetText;
-
     if (isEthSale) {
         tweetText = `${tokenName} bought for ${formattedEthPrice}Ξ ($${formattedUsdPrice}) #NFT ${openseaLink}`;
     } else {
         tweetText = `${tokenName} bought for ${formattedUsdPrice} ${tokenSymbol} #NFT ${openseaLink}`;
     }
-
     console.log(tweetText);
-
     return tweet.tweet(tweetText);
 }
 
 // Poll OpenSea every 60 seconds & retrieve all sales for a given collection in either the time since the last sale OR in the last minute
 setInterval(() => {
-    const lastSaleTime = cache.get('lastSaleTime', null) || moment().startOf('minute').subtract(59, "seconds").unix();
-
-    console.log(`Last sale (in seconds since Unix epoch): ${cache.get('lastSaleTime', null)}`);
-
+    const lastSaleTime = moment().subtract(65, "seconds").unix();
     axios.get('https://api.opensea.io/api/v1/events', {
         params: {
             collection_slug: process.env.OPENSEA_COLLECTION_SLUG,
@@ -50,21 +37,15 @@ setInterval(() => {
             only_opensea: 'false'
         }
     }).then((response) => {
+        console.log(response);
         const events = _.get(response, ['data', 'asset_events']);
-
         const sortedEvents = _.sortBy(events, function(event) {
-            const created = _.get(event, 'created_date');
-
+            var created = _.get(event, 'created_date');
             return new Date(created);
         })
 
         console.log(`${events.length} sales since the last one...`);
-
         _.each(sortedEvents, (event) => {
-            const created = _.get(event, 'created_date');
-
-            cache.set('lastSaleTime', moment(created).unix());
-
             return formatAndSendTweet(event);
         });
     }).catch((error) => {
